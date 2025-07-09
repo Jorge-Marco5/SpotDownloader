c6136956a572dcfae1f41c8f3e8168d543f772e5
Jorge_Marco5 <luismar1345@gmail.com>
2025-07-09 13:33:10 -0600

    Correccion de errores y de dependencias

--- requirements.py
+++ requirements.py
@@ -6,7 +6,7 @@ def instalar_paquetes():
     try:
         # Verifica si existe el archivo requirements.txt
         with open('requirements.txt', 'r') as file:
-            print("Instalando paquetes desde requirements.txt...")
+            print("Instalando paquetes desde requirements.txt, esto puede tomar un tiempo...")
             # Ejecuta el comando pip para instalar los paquetes
             subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
             print("¡Todos los paquetes han sido instalados correctamente!")
@@ -23,11 +23,12 @@ def crear_archivo_env(nombre_archivo=".env", variables=None):
 
     """Creacion del archivo de variables de entorno para la configuracion del proyecto.
     """
+    print("\n___________________________________")
     print("Creando archivo de configuración...\n")
     print("Si no tienes una cuenta de spotify, puedes crear una en https://www.spotify.com/signup/")
     print("Si no tienes una cuenta de spotify for developers, puedes crear una en https://developer.spotify.com/dashboard/login\n")
-    print("En la pagina de spotify for developers, crea una nueva app y obten tu client_id y client_secret, necesarios para acceder a la api de spotify")
-    print("tranquilo..., estos datos se almacenan de manera local, ni el creador del programa tiene conocimiento de ellos.")
+    print("En la pagina de spotify for developers, crea una nueva app y obten tu client_id y client_secret, necesarios para acceder a la api de spotify\n")
+    print("Tranquilo..., estos datos se almacenan de manera local, ni el creador del programa tiene conocimiento de ellos.")
     spotify_client_id = input("Spotify Client ID: ").strip()
     spotify_client_secret = input("Spotify Client Secret: ").strip()
 
@@ -40,18 +41,18 @@ def crear_archivo_env(nombre_archivo=".env", variables=None):
         with open(nombre_archivo, "w") as archivo:
             for clave, valor in variables.items():
                 archivo.write(f"{clave}={valor}\n")
-        print(f"Archivo '{nombre_archivo}' creado exitosamente.")
+        print(f"\nArchivo '{nombre_archivo}' creado exitosamente.\n")
     except Exception as e:
-        print(f"Error al crear el archivo: {e}")
+        print(f"\nError al crear el archivo: {e}\n")
 
 def main():
-    print("Bienvenido al instalador de paquetes de Spotify Playlist Downloader\n")
+    print("\nBienvenido al instalador de paquetes de Spotify Playlist Downloader\n")
     print("DESCARGO DE RESPONSABILIDAD:")
     print("Recuerda que preferiblemente descargues audios y videos que te pertenezcan o a los que tengas permiso.")
     print("Eres libre de hacer lo que gustes, el uso de este script es bajo TU RESPONSABILIDAD\n")
     print("Si tienes problemas con la instalación, asegúrate de tener pip instalado y actualizado.")
     print("Este script instalará todos los paquetes necesarios y configuraciones para ejecutar el proyecto.\n")
-    opcion = input("Antes de continuar necesitas una cuenta de spotify y registrarte en spotify for developers.\n ¿desea continuar?[y/n]: ").strip().lower()
+    opcion = input("Antes de continuar necesitas una cuenta de spotify y registrarte en spotify for developers.\n ¿Desea continuar?[y/n]: ").strip().lower()
     match opcion:
         case "y":
             instalar_paquetes()
