38222c7b8475ad75e4fc70c943bd6729e86cb08b
Jorge_Marco5 <luismar1345@gmail.com>
2025-07-09 18:44:55 -0600

    Correccion de errores y se agregan instrucciones para su instalacion

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-## SpotifyPlaylistDownloader 🎵
+
+# SpotDownloader
+
+Script que permite descargar musica y playlists a partir de una url de spotify, por la cual se obtienen los datos de la cancion y se descarga a traves de youtube en formato mp3 
+
+
+
+
+
+
+## Instalacion
+
+Requisitos:
+**Windows**
+* python
+
+Requisitos: **Linux**
+* python3
+* git
+* ffmpeg
+
+Resuisitos: **Android (Termux)** 
+* python3
+* git
+* ffmpeg
+
+---
+\
+Clone el projecto en una carpeta con:
+
+```bash
+  git clone url
+```
+
+## Ejemplo de uso
+
+Una vez dentro de la carpeta raiz del proyecto, la primera vez obtenemos los paquetes necesarios de python con:
+```bash
+  python requirements.py
+```
+* Despues de la instalacion iniciamos el programa con:
+```bash
+  python main.py
+```
+* Seleccionas si quieres descargar una playlist o una cancion
+
+```bash
+  ¿Que deseas descargar? [1 playlist| 2 cancion]: [opcion]
+```
+
+* Copias la url de tu cancion o playlist de spotify y la pegas en el programa
+
+```bash
+  Ingresa la URL de Spotify: [url]
+```
+
+* esperas la descarga y listo
+
+```bash
+  ¡Descarga completa!
+```
+
+## ¡Importante!
+
+No es necesario iniciar _requirements.py_ siempre, solo la primera vez
+
+Dentro del archivo _requirements.py_ vienen instrucciones mas detalladas para su instalacion dependiendo de tu sistema operativo
+
+Las canciones se guardan dentro de la carpeta _/music_ la cual se crea al terminar de configurar el proyecto
--- requirements.py
+++ requirements.py
@@ -4,10 +4,61 @@ from colorama import Fore, init
 import os
 init()
 
+"""
+09/07/2025
+
+Instalacion en windows:
+1-descargar e instalar python desde https://www.python.org/downloads/
+2-descargar e instalar git desde https://git-scm.com/downloads
+3-ir a la carpeta donde se clonara el repositorio y abrir una terminal
+4-clonar el repositorio con el comando: git clone https://github.com/Jorge-Marco5/SpotDownloader.git
+5-ingresar a la carpeta del repositorio con el comando: cd SpotDownloader
+6-Inicia el instalador de paquetes con el comando: python requirements.py
+7-Una ves que termine la instalacion, ejecuta el comado python main.py para iniciar el programa
+
+Instalacion en linux (Los comandos pueden variar dependiendo de la distribucion):
+1-abre una terminal y ejecuta los siguientes coomandos:
+2-sudo apt update -y && sudo apt upgrade -y
+3-sudo apt install python3-dev build-essential -y
+4-sudo apt install git -y
+4.1 instalar ffmpeg: apt install ffmpeg -y
+5-ir a la carpeta donde se clonara el repositorio
+6-clonar el repositorio con el comando: git clone https://github.com/Jorge-Marco5/SpotDownloader.git
+7-ingresar a la carpeta del repositorio con el comando: cd SpotDownloader
+8-Inicia el instalador de paquetes con el comando: python3 requirements.py
+9-una vez que termine la instalacion  ejecuta el comando python3 main.py para iniciar el programa
+
+Instalacion en Android con Termux:
+1-abre una terminal y ejecuta los siguientes comandos:
+2-actualizar terminal: pkg update -y && pkg upgrade -y
+3-instalar python: pkg install python -y
+4-instalar paqueteria necesaria de python: apt install python build-essential
+4.1 instalar ffmpeg: pkg install ffmpeg -y
+5-Instalar GIT: pkg install git -y
+6-dar permisos a la aplicacion para acceder al almacenamiento interno a traves del comando: termux-setup-storage
+7-ingresar a la siguiente ruta con el comando: cd storage/shared/
+8-crea una carpeta nueva; mkdir NombreCarpeta
+9-ingresar a la carpeta: cd NombreCarpeta
+10-clonar el repositorio con el comando: git clone https://github.com/Jorge-Marco5/SpotDownloader.git
+11-ingresar a SpotDownloader: cd SpotDownloader
+12-Inicia el instalador de paquetes con el comando: python requirements.py
+13-Una ves que termine la instalacion, ejecuta el comado python main.py para iniciar el programa
+14 instala: pip install colorama
+15 si aparece un error al descargar la musica descarga: pip install ffmpeg-python
+
+IMPORTANTE:
+En caso de obtener un error al momento de obtener los metadatos de la cancion, descarga el paquete ffmpeg
+apt install ffmpeg -y
+
+moverse entre carpetas en la terminal:
+para regresar una carpeta   cd ..
+para ingresar a una carpeta cd NombreCarpeta
+"""
+
 def instalar_paquetes():
     crear_archivo_env()
     try:
-        print("\nIniciando descarga de los paquetes. Esto puede tomar tiempo...\n")
+        print(Fore.CYAN+"\nIniciando descarga de los paquetes. Esto puede tomar tiempo...\n"+Fore.WHITE)
         # Ejecuta el comando pip para instalar los paquetes
         subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
         if not os.path.exists("music"):
@@ -57,7 +108,7 @@ def main():
     print(Fore.WHITE +"Recuerda que preferiblemente descargues audios y videos que te pertenezcan o a los que tengas permiso.")
     print("Eres libre de hacer lo que gustes, el uso de este script es bajo TU RESPONSABILIDAD\n")
     print("Si tienes problemas con la instalación, asegúrate de tener pip instalado y actualizado.")
-    print("apt install python3 build-essential\n")
+    print("'apt install python3 build-essential' ó 'pip install build-essential'\n")
     print("Este script instalará los siguientes paquetes necesarios y configuraciones para ejecutar el proyecto.\n")
     #mostrar los paquetes a instalar de requirements.txt
     with open("requirements.txt", "r", encoding="utf-8") as archivo:
